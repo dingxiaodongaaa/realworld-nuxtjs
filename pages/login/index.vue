4827d4bcadcb74e167c344ff6b2fc9ddef754c61
dingxd <dingxiaodongaaa@163.com>
2020-10-23 14:34:13 +0800

    首次提交

---
+++ pages/login/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">{{isLogin ? 'Sign in' : 'Sign up'}}</h1>
+          <p class="text-xs-center">
+            <nuxt-link
+              v-if="!isLogin"
+              to="/login">Have an account?
+            </nuxt-link>
+            <nuxt-link
+              v-else 
+              to="/register">Need an account?
+            </nuxt-link>
+          </p>
+
+          <ul class="error-messages">
+            <template v-for="(messages, feild) in errors">
+              <li v-for="(message, index) in messages" :key="index">{{ feild }} {{ message }}</li>
+            </template>
+          </ul>
+
+          <form @submit.prevent="onSubmit">
+            <fieldset v-if="!isLogin" class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Your Name"
+                v-model="user.username"
+                required
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+                v-model="user.email"
+                required
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+                v-model="user.password"
+                required
+                minlength="8"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              {{isLogin ? 'Sign in' : 'Sign up'}}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { login, register } from '@/api/user'
+
+// 只有在客户端运行时才会加载 js-cookie
+// process.client 时 Nuxt.js 提供的一个状态，用于判断当前是否运行在客户端
+const Cookie = process.client ? require('js-cookie') : undefined
+
+export default {
+  middleware: ['notAuthenticated'],
+  name: 'loginIndex',
+  computed: {
+    isLogin () {
+      return this.$route.name === 'login'
+    }
+  },
+  data () {
+    return {
+      user: {
+        username: '',
+        password: '',
+        email: ''
+      },
+      errors: {}
+    }
+  },
+  methods: {
+    async onSubmit () {
+      try {
+        // 提交表单请求登录
+        const { data } = this.isLogin
+          ? await login({
+            user: this.user
+          })
+          : await register({
+            user: this.user
+          })
+
+        console.log(data)
+        // TODO：保存用户的登录状态
+        this.$store.commit('setUser', data.user)
+
+        // 数据持久化
+        Cookie.set('user', data.user)
+
+        // 跳转到首页
+        this.$router.push('/')
+      } catch (err) {
+        console.dir(err)
+        this.errors = err.response.data.errors
+        console.log(this.errors)
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ store/index.js
@@ -0,0 +1,38 @@
+const cookieparser = process.server ? require('cookieparser') : undefined
+
+// 这里不需要重新创建实例，直接定义并导出各模块就可以了，nuxt 会加载并注册到容器中。
+// 在服务端渲染期间运行的都是同一个实例，为防止数据冲突，务必要把 state 定义成一个函数返回数据对象的形式。
+export const state = () => {
+  return {
+    // 当前登录用户的登录状态
+    user: null
+  }
+}
+
+export const mutations = {
+  setUser (state, data) {
+    state.user = data
+  }
+}
+
+export const actions = {
+  // nuxtServerInit 是 Nuxt.js 中一个特殊的 action 方法，这个 action 会在服务端渲染期间自动调用，且仅在服务端渲染期间调用
+  // 作用：初始化容器数据，传递数据给客户端使用
+  nuxtServerInit ({ commit }, { req }) {
+    let user = null
+    
+    // 如果请求头中由 cookie
+    if (req.headers.cookie) {
+      // 使用 cookieparser 把 cookie 字符串转换为 js 对象
+      const parsed = cookieparser.parse(req.headers.cookie)
+      try {
+        user = JSON.parse(parsed.user)
+      } catch (err) {
+        // No valid cookie found
+      }
+    }
+
+    // 提交 mutation 修改 state 状态
+    commit('setUser', user)
+  }
+}
